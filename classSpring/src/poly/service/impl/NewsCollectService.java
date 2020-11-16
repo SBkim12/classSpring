c086e593bee5357d74b953ba119440393cc7fdfd
SBKim12 <mar5924@naver.com>
2020-11-16 16:26:25 +0900

    한이음 끝남오랜만에 올림

---
+++ classSpring/src/poly/service/impl/NewsCollectService.java
@@ -0,0 +1,35 @@
+package poly.service.impl;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+
+import poly.service.INewsCollectService;
+
+@Service("NewsCollectService")
+public class NewsCollectService implements INewsCollectService {
+
+	@Override
+	public String doNaverNewsContents(String url) throws Exception{
+		
+		// JSOUP 라이브러리를 통해 사이트 접속 되면, 그 사이트의 전체 HTML소스 저장할 변수
+		Document doc = null;
+		
+		// 사이트 접속(http포로토콜만 가능, https 포로토콜은 보안상 안됨)
+		doc = Jsoup.connect(url).get();
+		
+		//네이버 뉴스 본문 내용에 대한 div 소스를 가져옴
+		// <div id="articleBodyContents" class="_article_body_contents">
+		Elements newsContent = doc.select("div._article_body_contents");
+		
+		
+		//태그 내 텍스트 문구만 가져오기
+		String res = newsContent.text();
+		
+		doc = null;
+		
+		return res;
+	}
+}
